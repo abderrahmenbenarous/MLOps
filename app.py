658d1856e95aad086c1bc9dab4ad5f003182e651
abderrahmenbenarous <abderrahmen.benarous@esprit.tn>
2024-11-13 15:15:24 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI
+from Customer import Customer
+import numpy as np
+import joblib
+import pandas as pd
+# 2. Create the app object
+app = FastAPI()
+joblib_in = open("classifier.joblib","rb")
+classifier=joblib.load(joblib_in)
+
+# 3. Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
+
+
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted Bank Note with the confidence
+#http://127.0.0.1:8000/predict
+@app.post('/predict')
+def predict_Customer(data:Customer):
+    data = data.dict()
+    CreditScore=data['CreditScore']
+    Gender=data['Gender']
+    Age =data['Age']
+    Tenure=data['Tenure']
+    Balance=data['Balance']
+    NumOfProducts=data['NumOfProducts']
+    HasCrCard =data['HasCrCard']
+    IsActiveMember=data['IsActiveMember']
+    EstimatedSalary = data['EstimatedSalary']
+
+   # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
+    prediction = classifier.predict([[	CreditScore	,Gender	,Age,	Tenure,	Balance	,NumOfProducts,	HasCrCard,	IsActiveMember	,EstimatedSalary]])
+    if(prediction[0]>0.5):
+        prediction="Customer will churn"
+    else:
+        prediction="Customer will not churn"
+    return {
+        'prediction': prediction
+    }
+
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
